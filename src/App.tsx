0db58a88781300efa8c658969ecdbc3cfc3d4443
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-08-24 00:41:57 -0300

    se pasa a typescipt y se agrega useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
+import { useReducer } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { useCart } from "./hooks/useCart"
+import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
   const { data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal } =  useCart()
+  const [state, dispatch] = useReducer(cartReducer, initialState)
 
-
+  console.log(state)
 
   return (
     <>
